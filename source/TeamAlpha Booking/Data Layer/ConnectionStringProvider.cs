04e5bf8179ab7769852fdb223e4062ac9220e37e
Bogdan Milojević <bogi.milojevic@gmail.com>
2022-01-03 00:26:51 +0100

    Created utility class for easy connection string lookup

---
+++ source/TeamAlpha Booking/Data Layer/ConnectionStringProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Layer
+{
+    public class ConnectionStringProvider
+    {
+        public static string GetConnectionString (String name) // returns connection string using database name
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
