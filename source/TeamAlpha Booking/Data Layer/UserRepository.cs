87c673c70c3df20f5259c30ce872fd7cffc6f421
Veljko Lončarević <62267658+theharoold@users.noreply.github.com>
2022-01-09 16:50:18 +0100

    Removed IsEmailUnique()

--- source/TeamAlpha Booking/Data Layer/UserRepository.cs
+++ source/TeamAlpha Booking/Data Layer/UserRepository.cs
@@ -104,13 +104,6 @@ namespace Data_Layer
             }
         }
         
-        public bool IsEmailUnique(String email)
-        {
-            using (SqlConnection connection = new SqlConnection(ConnectionStringProvider.GetConnectionString("AlphaBookingDB")))
-            {
-                return connection.Execute("SELECT COUNT(*) FROM Korisnici WHERE Email = @Email", new { Email = email }) == 0 ? true : false ;
-            }
-        }
     }
 }
 
