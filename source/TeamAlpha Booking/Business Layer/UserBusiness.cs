3f5ec500876b2557512407e4c7737d2e5024c656
Bogdan Milojević <bogi.milojevic@gmail.com>
2022-01-09 12:15:12 +0100

    Implemented method for user registration

--- source/TeamAlpha Booking/Business Layer/UserBusiness.cs
+++ source/TeamAlpha Booking/Business Layer/UserBusiness.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Shared.Models;
+using Data_Layer;
 
 namespace Business_Layer
 {
-    class UserBusiness
+    public class UserBusiness
     {
+
+        private readonly UserRepository userRepository;
+        
+        public int  RegisterUser (User user) // pass validated instance
+        {
+            String email = user.Email;
+
+            if (userRepository.IsEmailUnique(email)) { // check if entered email is unique  
+                return userRepository.InsertUser(user); //insert new user into db
+            }
+
+            return -1;
+        }
     }
 }
