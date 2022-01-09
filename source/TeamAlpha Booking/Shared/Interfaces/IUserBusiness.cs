939ebd6e87e4d0314a8d089345c67b182ec2f812
Bogdan Milojević <bogi.milojevic@gmail.com>
2022-01-09 18:56:45 +0100

    Added Interface for UserBusiness

---
+++ source/TeamAlpha Booking/Shared/Interfaces/IUserBusiness.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.Models;
+
+namespace Shared.Interfaces
+{
+    public interface IUserBusiness
+    {
+        int RegisterUser(User user);
+        Boolean CheckPassword(string email, string password);
+    }
+}
