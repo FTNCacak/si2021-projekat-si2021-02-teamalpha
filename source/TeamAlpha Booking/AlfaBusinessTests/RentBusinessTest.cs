5cf08ef81e3d67b0eec34cfacfe5dfa7957c5148
Savex00 <savos977@gmail.com>
2022-01-13 13:39:11 +0100

    Unit test folder and business layer

---
+++ source/TeamAlpha Booking/AlfaBusinessTests/RentBusinessTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+namespace AlfaBusinessTests
+{
+    [TestClass]
+    public class RentBusinessTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+        }
+    }
+}
